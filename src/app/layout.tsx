be59c0814dc5207d2240d56e0c52d8e850d3ea5c
anggakswr <kamiswara98@gmail.com>
2024-10-22 16:58:40 +0700

    feat: posts list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import CustomProviders from "@/components/CustomProviders";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="bg-gray-500">
+          <div className="mx-auto min-h-screen w-96 bg-white p-4">
+            <CustomProviders>{children}</CustomProviders>
+          </div>
+        </div>
       </body>
     </html>
   );
